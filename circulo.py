e14ad5690c5acf0de14b98354075c4cb7fe8ad92
vale <l22121064@morelia.tecnm.mx>
2024-09-11 23:13:47 -0600

    Tarea terminada

--- circulo.py
+++ circulo.py
@@ -7,3 +7,7 @@ class Circulo:
     def calcular_area(self):
         area = (self.radio * self.radio * 3.1416)
         print("Área =", area)
+
+    def calcular_perimetro(self):
+        perimetro = (self.radio * 2 * 3.1416)
+        print("Perimetro =", perimetro)
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from circulo import Circulo 
-radio = int(input("\n Escribir Radio del Circulo \n"))
-circulo = Circulo(radio)
-circulo.calcular_area()
 
+while True:
+    print("\n*** BIENVENIDO ***")
+    print("Opciones en el Sistema")
+    print("1. Calcular Area")
+    print("2. Calcular Perimetro")
+    print("3. Salir")
 
+    opcion = input("Ingresa la opción que deseas: ")
+
+    if opcion == "1":
+        radio = int(input("\n Escribir Radio del Circulo \n"))
+        circulo = Circulo(radio)
+        circulo.calcular_area()
+    elif opcion == "2":
+        radio = int(input("\n Escribir Radio del Circulo \n"))
+        circulo = Circulo(radio)
+        circulo.calcular_perimetro()
+    elif opcion == "3":
+        print("\nHasta luego")
+        break
