222f10c0e9dfad858aaba865826dc562c1b9cf21
vale <l22121064@morelia.tecnm.mx>
2024-09-24 23:51:44 -0600

    avance 1

--- grupo/grupo.py
+++ grupo/grupo.py
@@ -1,2 +1,10 @@
+from typing import List
+from estudiantes.estudiantes import Estudiante
+from datetime import datetime
+from maestros.maestros import Maestro
+from materias.materias import Materia
+
 class Grupo: 
-    pass
+    id: int
+    #faltan cosas
+    estudiantes: List[Estudiante]
