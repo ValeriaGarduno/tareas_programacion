f475d68e4dd93a8f96325a1152daef5bbcead4b5
vale <l22121064@morelia.tecnm.mx>
2024-10-03 23:35:29 -0600

    LISTO

--- coordinador/coordinador.py
+++ coordinador/coordinador.py
@@ -3,15 +3,15 @@ from usuario.usuario import Usuario
 from usuario.utils.roles import Rol
 
 class Coordinador(Usuario):
-    curp: str
-    fecha_nacimiento: datetime
+    rfc: str
+    ano_antiguedad: datetime
     
-    def __init__(self, numero_control, nombre: str, apellido: str, curp: str, fecha_nacimiento: datetime, contrasenia: str, rol: Rol):
-        super().__init__(numero_control=numero_control, nombre=nombre, apellido=apellido, contrasenia=contrasenia, rol=Rol.ESTUDIANTE)
+    def __init__(self, numero_control, nombre: str, apellido: str, curp: str, ano_antiguedad: datetime, contrasenia: str, rol: Rol):
+        super().__init__(numero_control=numero_control, nombre=nombre, apellido=apellido, contrasenia=contrasenia, rol=Rol.COORDINADOR)
         self.curp = curp
-        self.fecha_nacimiento = fecha_nacimiento
+        self.ano_antiguedad = ano_antiguedad
         
     def mostrar_info_coordinador(self):
         nombre_completo = f"{self.nombre} {self.apellido}"
-        info = f"Numero de control: {self.numero_control}, Nombre Completo: {nombre_completo}, Curp: {self.curp}, Fecha de Nacimiento: {self.fecha_nacimiento}, ROL: {self.rol.value}"
+        info = f"Numero de control: {self.numero_control}, Nombre Completo: {nombre_completo}, Curp: {self.curp}, AñO ANTIGUEDAD: {self.ano_antiguedad}, ROL: {self.rol.value}"
         return info
