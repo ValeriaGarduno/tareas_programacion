f2607eecd5c9b3877f9bdb59a727a3f641f0b4fc
vale <l22121064@morelia.tecnm.mx>
2024-10-27 14:14:27 -0600

    done

--- materias/materias.py
+++ materias/materias.py
@@ -1,12 +1,14 @@
 class Materia: 
 
-    def __init__(self,nombrem: str, instructor: str, descripcion: str, semestre: int, creditos: int, id):
+    def __init__(self,nombrem: str, instructor: str, descripcion: str, semestre: int, creditos: int, id, maestro: Maestro):
         self.nombrem = nombrem
         self.instructor = instructor
         self.descripcion = descripcion
         self.semestre = semestre
         self.creditos = creditos
         self.id = id 
+        self.maestro = Maestro
+
 
     def mostrar_info_materias(self):
         info = f"Instructor: {self.instructor}, Nombre Completo: {self.nombrem}, Descripcion: {self.descripcion}, Semestre: {self.semestre},ID: {self.id}"
